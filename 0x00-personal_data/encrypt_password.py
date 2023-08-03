7095cbd654ca657c206282912a762c910eab9c35
lordzac <balogunzacchaeus@gmail.com>
2023-08-03 23:56:27 +0100

    upload file

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-""" Returns a salted, hashed password, byte in string """
+""" Encrypting pswds with bcrypt """
+
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns byte string password """
+    """ Takes in string arg, converts to unicode
+    Returns salted, hashed pswd as bytestring
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Implement is_valid to validate provided password
-    matched hashed_password
+    """ Checks if hashed and unhashed pswds are same
+    Returns bool
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
